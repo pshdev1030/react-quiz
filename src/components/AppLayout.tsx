79086cf5995b9b97fb126c888f72180ef20c9280
ParkSungHyun <psh1030.dev@gmail.com>
2022-07-13 08:31:16 +0900

    Feat: 공통 레이아웃 설정
    모든 페이지에서 사용할 AppLayout 컴포넌트를 설정하였습니다.

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
+import styled from "@emotion/styled";
 const AppLayout = () => {
   return (
-    <div>
-      AppLayout
+    <AppLayoutContainer>
       <Outlet />
-    </div>
+    </AppLayoutContainer>
   );
 };
 
 export default AppLayout;
+
+const AppLayoutContainer = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(to bottom, #a1c4fd 0%, #c2e9fb 100%); ;
+`;
