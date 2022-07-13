e1f9790455444334c5302689b7c786b8cf65b970
ParkSungHyun <psh1030.dev@gmail.com>
2022-07-13 09:07:49 +0900

    Feat: question 타입 지정
    question 타입을 지정하였습니다.

---
+++ src/types/db.ts
@@ -0,0 +1,9 @@
+export interface QuestionType {
+  select_array: string[];
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
