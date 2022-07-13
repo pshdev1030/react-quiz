c2d85308639ad63b4768929d75eab93b50a5ad66
ParkSungHyun <psh1030.dev@gmail.com>
2022-07-13 11:51:29 +0900

    Feat: review 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
 import Question from "./pages/Question";
 import Result from "./pages/Result";
+import Review from "./pages/Review";
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +14,7 @@ function App() {
           <Route index element={<Main />}></Route>
           <Route path="question/:id" element={<Question />}></Route>
           <Route path="result" element={<Result />} />
+          <Route path="review/:id" element={<Review />}></Route>
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
